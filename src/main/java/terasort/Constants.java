f161489a8a900ca678c512e8f6bbd89b16018b1d
Rajesh Balamohan <rbalamohan@apache.org>
2015-07-27 13:02:55 +0530

    Commiting Constants and few changes to pom.xml

---
+++ src/main/java/terasort/Constants.java
@@ -0,0 +1,30 @@
+package terasort;
+
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+public class Constants {
+
+  public static final String SAMPLE_SIZE = "terasort.partitions.sample";
+  public static final String NUM_PARTITIONS = "terasort.num.partitions";
+
+  public static String OUTPUT_REPLICATION = "terasort.output.replication";
+
+  public static final String FINAL_SYNC_ATTRIBUTE = "terasort.final.sync";
+
+  public static final String NUM_ROWS = "terasort.num-rows";
+}
